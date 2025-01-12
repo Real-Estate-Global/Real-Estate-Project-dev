9141b65b4a1689f4090032784b484ba12fb27c7c
abtv23 <aniboteva11@gmail.com>
2025-01-12 17:15:00 +0200

    design changes

--- Client/src/components/OfferDetails/OfferDetails.tsx
+++ Client/src/components/OfferDetails/OfferDetails.tsx
@@ -1,5 +1,5 @@
 import { OfferType } from "../../types/OfferType";
-import styles from "./OfferDetails.module.css";
+// import styles from "./OfferDetails.module.css";
 import { DateTime } from 'luxon'
 
 type Props = {
@@ -12,36 +12,57 @@ export const OfferDetails: React.FC<Props> = ({ offerDetails }) => {
   }
 
   return (
-    <div className={styles["offer-details-page"]}>
-      <h1 className={styles["offer-title"]}>{offerDetails.propertyType}</h1>
-      <div className={styles["offer-details"]}>
-        <div className={styles["offer-characteristics"]}>
-          <div className={styles["photo-container"]}>
-            <img
+    <div className="offer-details-page">
+      <h1 className="offer-title">{offerDetails.propertyType}</h1>
+      <div className="offer-details">
+        <div className="offer-characteristics">
+          {/* <div className=""> */}
+            {/* <img
               className={styles["property-photo"]}
               src={offerDetails.img}
               alt="apartment photo"
-            />
-          </div>
-          <ul className={styles["offer-specs"]}>
-            <li className={styles["right-positioned-specs"]}>
+            /> */}
+            <div className="collage-container">
+              <div className="collage-item large-image">
+                <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt="Building exterior" />
+              </div>
+              <div className="collage-item small-image">
+                <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt="Living room view" />
+              </div>
+              <div className="collage-item small-image">
+                <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt="Dining area" />
+              </div>
+              <div className="collage-item small-image">
+                <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt="Bedroom" />
+              </div>
+              <div className="collage-item small-image">
+                <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt="Bathroom" />
+              </div>
+              <div className="collage-item small-image">
+                <img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg" alt="Second bedroom" />
+              </div>
+            </div>
+          {/* </div> */}
+          
+        </div>
+        <div className="offer-description-li">
+          <p className="offer-description-page">
+            {offerDetails.description}
+          </p>
+          <ul className="offer-specs">
+            <li className="right-positioned-specs">
               {offerDetails.location}, {offerDetails.district}
             </li>
             <li
-              className={styles["right-positioned-specs"]}
+              className="right-positioned-specs"
             >{` Цена: ${offerDetails.price} ${offerDetails.currency === "EUR" ? " €" : " лв."}`}</li>
             <li
-              className={styles["right-positioned-specs"]}
+              className="right-positioned-specs"
             >{`Площ: ${offerDetails.area} кв.м.`}</li>
             <li
-              className={styles["right-positioned-specs"]}
+              className="right-positioned-specs"
             >{`Година на строителство: ${offerDetails.yearOfBuilding}`}</li>
           </ul>
-        </div>
-        <div className={styles["offer-description-li"]}>
-          <p className={styles["offer-description-page"]}>
-            {offerDetails.description}
-          </p>
         </div>
       </div>
     </div>
