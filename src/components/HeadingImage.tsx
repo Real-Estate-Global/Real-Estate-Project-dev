4921f2221cfb955333cbc651a1a0bb406995cbbc
abtv23 <aniboteva11@gmail.com>
2025-02-28 10:46:13 +0200

    fix icons serve

--- src/components/HeadingImage.tsx
+++ src/components/HeadingImage.tsx
@@ -5,7 +5,7 @@
 export const HeadingImage = () => {
   return (
     <div className="heading-image-wrapper">
-      <img className="heading-image" src="../../public/heading-home-image.png"></img>
+      <img className="heading-image" src="./heading-home-image.png"></img>
       {/* <h1 className="heading-image-title">Намери своето жилище сега!</h1> */}
       {/* <SearchForm getHomeOfferList={getHomeOfferList} /> */}
     </div>
